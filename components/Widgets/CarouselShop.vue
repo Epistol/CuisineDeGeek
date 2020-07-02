e627d49387068289fe08e4a2883067db631678ef
Michaël Lebeau <mikaleb@live.fr>
2020-07-02 16:00:16 +0200

    feat(carousel): set  href to shop

--- components/Widgets/CarouselShop.vue
+++ components/Widgets/CarouselShop.vue
@@ -3,14 +3,21 @@
     <template>
       <v-card-title class="font-weight-bold">{{ $t('common.ads.shop') }}</v-card-title>
       <v-card-text style="min-height:150px;min-width:150px">
-        <v-carousel cycle interval="12000" height="400" hide-delimiter-background show-arrows-on-hover v-if="products">
+        <v-carousel
+          cycle
+          interval="12000"
+          height="400"
+          hide-delimiter-background
+          show-arrows-on-hover
+          v-if="products"
+        >
           <v-carousel-item
             v-for="(product, i) in products.products"
             :key="i"
             :src="product.image.src"
+            :href="'https://shop.cuisinedegeek.com/'"
             transition="fade-transition"
-          >
-          </v-carousel-item>
+          ></v-carousel-item>
         </v-carousel>
       </v-card-text>
     </template>
